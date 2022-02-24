43fb0d1ef776fe68250b70864e0f8d8cbfbd114f
Ajna Moreira <ajna.moreira@gmail.com>
2022-02-24 18:54:24 -0300

    first commit

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { TextField, Button, Switch, FormControlLabel } from '@mui/material'
+
+function FormularioCadastro() {
+    return (
+        <form>
+            <TextField
+                id="nome"
+                label="Nome"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                id="sobrenome"
+                label="Sobrenome"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                id="cpf"
+                label="CPF"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+
+            <FormControlLabel
+                control={<Switch name="promocoes" defaultChecked />}
+                label="Promoções"
+            />
+
+            <FormControlLabel
+                control={<Switch name="novidades" defaultChecked />}
+                label="Novidades"
+            />
+
+            <Button type="submit" variant="contained" color="primary">
+                Cadastrar
+            </Button>
+        </form>
+    )
+}
+
+export default FormularioCadastro
