2a22df14ce2f01738853fb4347e1dd54a1202748
Ajna Moreira <ajna.moreira@gmail.com>
2022-02-28 10:47:06 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,32 @@ class App extends Component {
     render() {
         return (
             <Container component="article" maxWidth="sm">
-                <Typography variant="h3" component="h1" align="center">
+                <Typography
+                    variant="h3"
+                    component="h1"
+                    align="center"
+                    sx={{ margin: 2 }}
+                >
                     Formulário de cadastro
                 </Typography>
-                <FormularioCadastro />
+                <FormularioCadastro
+                    aoEnviar={aoEnviarForm}
+                    validarCPF={validaCPF}
+                />
             </Container>
         )
+    }
+}
+
+function aoEnviarForm(dados) {
+    console.log(dados)
+}
+
+function validaCPF(cpf) {
+    if (cpf.length !== 11) {
+        return { valido: false, texto: 'O CPF deve ter 11 digitos' }
+    } else {
+        return { valido: true, texto: '' }
     }
 }
 
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { TextField, Button, Switch, FormControlLabel } from '@mui/material'
+import React, { useState } from 'react'
+import {
+    TextField,
+    Button,
+    Switch,
+    FormControlLabel,
+    formControlLabelClasses
+} from '@mui/material'
 
-function FormularioCadastro() {
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [nome, setNome] = useState('')
+    const [sobrenome, setSobrenome] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [promocoes, setPromocoes] = useState(true)
+    const [novidades, setNovidades] = useState(true)
+
+    const [erros, setErros] = useState({ cpf: { valido: true, texto: '' } })
+
     return (
-        <form>
+        <form
+            onSubmit={event => {
+                event.preventDefault()
+                aoEnviar({ nome, sobrenome, cpf, promocoes, novidades })
+            }}
+        >
             <TextField
+                value={nome}
+                onChange={event => {
+                    setNome(event.target.value)
+                }}
                 id="nome"
                 label="Nome"
                 variant="outlined"
                 fullWidth
-                margin="normal"
             />
 
             <TextField
+                value={sobrenome}
+                onChange={event => {
+                    setSobrenome(event.target.value)
+                }}
                 id="sobrenome"
                 label="Sobrenome"
                 variant="outlined"
@@ -21,6 +47,18 @@ function FormularioCadastro() {
             />
 
             <TextField
+                value={cpf}
+                onChange={event => {
+                    setCpf(event.target.value)
+                }}
+                onBlur={event => {
+                    const ehValido = validarCPF(cpf)
+                    setErros({
+                        cpf: ehValido
+                    })
+                }}
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
                 id="cpf"
                 label="CPF"
                 variant="outlined"
@@ -29,12 +67,28 @@ function FormularioCadastro() {
             />
 
             <FormControlLabel
-                control={<Switch name="promocoes" defaultChecked />}
+                control={
+                    <Switch
+                        checked={promocoes}
+                        onChange={event => {
+                            setPromocoes(event.target.checked)
+                        }}
+                        name="promocoes"
+                    />
+                }
                 label="Promoções"
             />
 
             <FormControlLabel
-                control={<Switch name="novidades" defaultChecked />}
+                control={
+                    <Switch
+                        checked={novidades}
+                        onChange={event => {
+                            setNovidades(event.target.checked)
+                        }}
+                        name="novidades"
+                    />
+                }
                 label="Novidades"
             />
 
